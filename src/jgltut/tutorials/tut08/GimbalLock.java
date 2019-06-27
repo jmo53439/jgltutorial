7e939a93bebf7286b63eb9da954d59e66943e92c
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-06-27 13:20:46 -0500

    Create GimbalLock.java

---
+++ src/jgltut/tutorials/tut08/GimbalLock.java
@@ -0,0 +1,235 @@
+package jgltut.tutorials.tut08;
+
+import jgltut.Tutorial;
+import jgltut.framework.Framework;
+import jgltut.framework.Mesh;
+import org.joml.Matrix4f;
+import org.joml.MatrixStackf;
+import org.joml.Vector4f;
+
+import java.util.ArrayList;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+
+public class GimbalLock extends Tutorial {
+    
+    private int theProgram;
+    private int modelToCameraMatrixUnif;
+    private int cameraToClipMatrixUnif;
+    private int baseColorUnif;
+    
+    private Matrix4f cameraToClipMatrix = new Matrix4f();
+    
+    private Mesh object;
+    private Mesh gimbals[] = new Mesh[3];
+    private String gimbalNames[] = {
+            
+            "LargeGimbal.xml",
+            "MediumGimbal.xml",
+            "SmallGimbal.xml"
+    };
+    
+    private GimbalAngles gimbalAngles = new GimbalAngles();
+    private boolean drawGimbals = true;
+    
+    private final float frustumScale = calcFrustumScale(20.0f);
+    
+    @Override
+    protected void init() {
+    
+        initializeProgram();
+        
+        for(int gimbalIndex = 0; gimbalIndex < 3; gimbalIndex++) {
+            
+            gimbals[gimbalIndex] = new Mesh(gimbalNames[gimbalIndex]);
+        }
+        
+        object = new Mesh("Ship.xml");
+        
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CW);
+        
+        glEnable(GL_DEPTH_TEST);
+        glDepthMask(true);
+        glDepthFunc(GL_LEQUAL);
+        glDepthRange(0.0f, 1.0f);
+        
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+           
+            if(action == GLFW_PRESS) {
+                
+                switch(key) {
+                
+                    case GLFW_KEY_SPACE:
+                        drawGimbals = !drawGimbals;
+                        break;
+                        
+                    case GLFW_KEY_ESCAPE:
+                        glfwSetWindowShouldClose(window, true);
+                        break;
+                }
+            }
+        });
+    }
+    
+    @Override
+    protected void display() {
+    
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearDepth(1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        MatrixStackf currMatrix = new MatrixStackf(10);
+        currMatrix.translate(0.0f, 0.0f, -200.0f);
+        
+        currMatrix.rotateX((float) Math.toRadians(gimbalAngles.angleX));
+        drawGimbal(currMatrix, GimbalAxis.X_AXIS, new Vector4f(0.4f, 0.4f, 1.0f, 1.0f));
+        currMatrix.rotateY((float) Math.toRadians(gimbalAngles.angleY));
+        drawGimbal(currMatrix, GimbalAxis.Y_AXIS, new Vector4f(0.0f, 1.0f, 0.0f, 1.0f));
+        currMatrix.rotateZ((float) Math.toRadians(gimbalAngles.angleZ));
+        drawGimbal(currMatrix, GimbalAxis.Z_AXIS, new Vector4f(1.0f, 0.3f, 0.3f, 1.0f));
+        
+        glUseProgram(theProgram);
+    
+        currMatrix.scale(3.0f, 3.0f, 3.0f);
+        currMatrix.rotateX((float) Math.toRadians(-90.0f));
+        
+        glUniform4f(baseColorUnif, 1.0f, 1.0f, 1.0f, 1.0f);
+        glUniformMatrix4fv(modelToCameraMatrixUnif, false, currMatrix.get(mat4Buffer));
+        
+        object.render("tint");
+        
+        glUseProgram(0);
+    }
+    
+    @Override
+    protected void reshape(int w, int h) {
+    
+        cameraToClipMatrix.m00(frustumScale * (h / (float) w));
+        cameraToClipMatrix.m11(frustumScale);
+        
+        glUseProgram(theProgram);
+        glUniformMatrix4fv(cameraToClipMatrixUnif, false, cameraToClipMatrix.get(mat4Buffer));
+        glUseProgram(0);
+        glViewport(0, 0, w, h);
+    }
+    
+    @Override
+    protected void update() {
+    
+        final float SMALL_ANGLE_INCREMENT = 9.0f;
+        final float scale = 10;
+        
+        if(isKeyPressed(GLFW_KEY_W)) {
+    
+            gimbalAngles.angleX += SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+        else if(isKeyPressed(GLFW_KEY_S)) {
+    
+            gimbalAngles.angleX -= SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+        
+        if(isKeyPressed(GLFW_KEY_A)) {
+    
+            gimbalAngles.angleY += SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+        else if(isKeyPressed(GLFW_KEY_D)) {
+    
+            gimbalAngles.angleY -= SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+        
+        if(isKeyPressed(GLFW_KEY_Q)) {
+    
+            gimbalAngles.angleZ += SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+        else if(isKeyPressed(GLFW_KEY_E)) {
+    
+            gimbalAngles.angleZ -= SMALL_ANGLE_INCREMENT * lastFrameDuration * scale;
+        }
+    }
+    
+    private void initializeProgram() {
+    
+        ArrayList<Integer> shaderList = new ArrayList <>();
+    
+        shaderList.add(Framework.loadShader(GL_VERTEX_SHADER, "PosColorLocalTransform.vert"));
+        shaderList.add(Framework.loadShader(GL_FRAGMENT_SHADER, "ColorMultUniform.frag"));
+        theProgram = Framework.createProgram(shaderList);
+        
+        modelToCameraMatrixUnif = glGetUniformLocation(theProgram, "modelToCameraMatrix");
+        cameraToClipMatrixUnif = glGetUniformLocation(theProgram, "cameraToClipMatrix");
+        baseColorUnif = glGetUniformLocation(theProgram, "baseColor");
+        
+        float zNear = 1.0f;
+        float zFar = 600.0f;
+        
+        cameraToClipMatrix.m00(frustumScale);
+        cameraToClipMatrix.m11(frustumScale);
+        cameraToClipMatrix.m22((zFar + zNear) / (zNear - zFar));
+        cameraToClipMatrix.m23(-1.0f);
+        cameraToClipMatrix.m32((2 * zFar * zNear) / (zNear - zFar));
+        
+        glUseProgram(theProgram);
+        glUniformMatrix4fv(cameraToClipMatrixUnif, false, cameraToClipMatrix.get(mat4Buffer));
+        glUseProgram(0);
+    }
+    
+    private float calcFrustumScale(float fovDeg) {
+    
+        float fovRad = (float) Math.toRadians(fovDeg);
+        return (float)(1.0f / Math.tan(fovRad / 2.0f));
+    }
+    
+    private void drawGimbal(MatrixStackf currMatrix, GimbalAxis axis, Vector4f baseColor) {
+    
+        if(!drawGimbals)
+            return;
+        
+        currMatrix.pushMatrix();
+        
+        switch(axis) {
+            
+            case X_AXIS:
+                break;
+                
+            case Y_AXIS:
+                currMatrix.rotateZ((float) Math.toRadians(90.0f));
+                currMatrix.rotateX((float) Math.toRadians(90.0f));
+                break;
+                
+            case Z_AXIS:
+                currMatrix.rotateY((float) Math.toRadians(90.0f));
+                currMatrix.rotateX((float) Math.toRadians(90.0f));
+                break;
+        }
+        
+        glUseProgram(theProgram);
+        
+        glUniform4f(baseColorUnif, baseColor.x, baseColor.y, baseColor.z, baseColor.w);
+        glUniformMatrix4fv(modelToCameraMatrixUnif, false, currMatrix.get(mat4Buffer));
+        
+        gimbals[axis.ordinal()].render();
+        
+        glUseProgram(0);
+        currMatrix.popMatrix();
+    }
+    
+    public static void main(String[] args) {
+    
+        Framework.CURRENT_TUTORIAL_PATH = "/jgltut/tutorials/tut08/data/";
+        new GimbalLock().start(500, 500);
+    }
+    
+    private enum GimbalAxis {
+        
+        X_AXIS, Y_AXIS, Z_AXIS
+    }
+    
+    private class GimbalAngles {
+        
+        float angleX, angleY, angleZ;
+    }
+}
