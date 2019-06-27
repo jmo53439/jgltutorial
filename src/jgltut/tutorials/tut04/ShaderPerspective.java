adce40157c13d5459c3c0c6b5fb2f99bcb0ae805
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-06-27 12:22:19 -0500

    Create ShaderPerspective.java

---
+++ src/jgltut/tutorials/tut04/ShaderPerspective.java
@@ -0,0 +1,212 @@
+package jgltut.tutorials.tut04;
+
+import jgltut.Tutorial;
+import jgltut.framework.Framework;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+public class ShaderPerspective extends Tutorial {
+    
+    private int theProgram;
+    private int vertexBufferObject;
+    private int offsetUniform;
+    
+    private final float[] vertexData = {
+            
+            0.25f, 0.25f, -1.25f, 1.0f,
+            0.25f, -0.25f, -1.25f, 1.0f,
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            
+            0.25f, -0.25f, -1.25f, 1.0f,
+            -0.25f, -0.25f, -1.25f, 1.0f,
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            
+            0.25f, 0.25f, -2.75f, 1.0f,
+            -0.25f, 0.25f, -2.75f, 1.0f,
+            0.25f, -0.25f, -2.75f, 1.0f,
+            
+            0.25f, -0.25f, -2.75f, 1.0f,
+            -0.25f, 0.25f, -2.75f, 1.0f,
+            -0.25f, -0.25f, -2.75f, 1.0f,
+            
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            -0.25f, -0.25f, -1.25f, 1.0f,
+            -0.25f, -0.25f, -2.75f, 1.0f,
+            
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            -0.25f, -0.25f, -2.75f, 1.0f,
+            -0.25f, 0.25f, -2.75f, 1.0f,
+            
+            0.25f, 0.25f, -1.25f, 1.0f,
+            0.25f, -0.25f, -2.75f, 1.0f,
+            0.25f, -0.25f, -1.25f, 1.0f,
+            
+            0.25f, 0.25f, -1.25f, 1.0f,
+            0.25f, 0.25f, -2.75f, 1.0f,
+            0.25f, -0.25f, -2.75f, 1.0f,
+            
+            0.25f, 0.25f, -2.75f, 1.0f,
+            0.25f, 0.25f, -1.25f, 1.0f,
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            
+            0.25f, 0.25f, -2.75f, 1.0f,
+            -0.25f, 0.25f, -1.25f, 1.0f,
+            -0.25f, 0.25f, -2.75f, 1.0f,
+            
+            0.25f, -0.25f, -2.75f, 1.0f,
+            -0.25f, -0.25f, -1.25f, 1.0f,
+            0.25f, -0.25f, -1.25f, 1.0f,
+            
+            0.25f, -0.25f, -2.75f, 1.0f,
+            -0.25f, -0.25f, -2.75f, 1.0f,
+            -0.25f, -0.25f, -1.25f, 1.0f,
+            
+            
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            
+            0.8f, 0.8f, 0.8f, 1.0f,
+            0.8f, 0.8f, 0.8f, 1.0f,
+            0.8f, 0.8f, 0.8f, 1.0f,
+            
+            0.8f, 0.8f, 0.8f, 1.0f,
+            0.8f, 0.8f, 0.8f, 1.0f,
+            0.8f, 0.8f, 0.8f, 1.0f,
+            
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            
+            0.5f, 0.5f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f, 1.0f,
+            
+            0.5f, 0.5f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.0f, 1.0f,
+            
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            
+            0.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f,
+            
+            0.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f
+    };
+    
+    @Override
+    protected void init() {
+    
+        initializeProgram();
+        initializeVertexBuffer();
+    
+        int vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+    
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CW);
+    }
+    
+    @Override
+    protected void display() {
+    
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    
+        glUseProgram(theProgram);
+    
+        glUniform2f(offsetUniform, 0.5f, 0.5f);
+    
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(0, 4, GL_FLOAT, false, 0, 0);
+    
+        int colorData = (Float.BYTES * vertexData.length) / 2;
+        glVertexAttribPointer(1, 4, GL_FLOAT, false, 0, colorData);
+    
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glUseProgram(0);
+    }
+    
+    @Override
+    protected void reshape(int w, int h) {
+    
+        glViewport(0, 0, w, h);
+    }
+    
+    @Override
+    protected void update() {
+    
+    }
+    
+    private void initializeProgram() {
+    
+        ArrayList<Integer> shaderList = new ArrayList <>();
+        shaderList.add(Framework.loadShader(GL_VERTEX_SHADER, "ManualPerspective.vert"));
+        shaderList.add(Framework.loadShader(GL_FRAGMENT_SHADER, "StandardColors.frag"));
+        theProgram = Framework.createProgram(shaderList);
+        
+        offsetUniform = glGetUniformLocation(theProgram, "offset");
+        
+        glUseProgram(theProgram);
+        
+        int frustumScaleUnif = glGetUniformLocation(theProgram, "frustumScale");
+        glUniform1f(frustumScaleUnif, 1.0f);
+        
+        int zNearUnif = glGetUniformLocation(theProgram, "zNear");
+        glUniform1f(zNearUnif, 1.0f);
+        
+        int zFarUnif = glGetUniformLocation(theProgram, "zFar");
+        glUniform1f(zFarUnif, 3.0f);
+        
+        glUseProgram(0);
+    }
+    
+    private void initializeVertexBuffer() {
+        
+        FloatBuffer vertexDataBuffer = BufferUtils.createFloatBuffer(vertexData.length);
+        vertexDataBuffer.put(vertexData);
+        vertexDataBuffer.flip();
+        
+        vertexBufferObject = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+        glBufferData(GL_ARRAY_BUFFER, vertexDataBuffer, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    
+    public static void main(String[] args) {
+    
+        Framework.CURRENT_TUTORIAL_PATH = "/jgltut/tutorials/tut04/data/";
+        new ShaderPerspective().start(500, 500);
+    }
+}
