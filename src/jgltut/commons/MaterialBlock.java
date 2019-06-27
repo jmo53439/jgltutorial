44ec8aa3672822957083c160825c26c3a9848921
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-06-27 11:39:43 -0500

    Create MaterialBlock.java

---
+++ src/jgltut/commons/MaterialBlock.java
@@ -0,0 +1,33 @@
+package jgltut.commons;
+
+import org.joml.Vector4f;
+
+import java.nio.ByteBuffer;
+
+public class MaterialBlock implements Bufferable {
+    
+    public static final int BYTES = Float.BYTES * (4 + 4 + 1 + 3);
+    
+    public Vector4f diffuseColor;
+    public Vector4f specularColor;
+    public float specularShininess;
+    public float padding[] = new float[3];
+    
+    @Override
+    public ByteBuffer get(ByteBuffer buffer) {
+        
+        diffuseColor.get(buffer);
+        buffer.position(buffer.position() + Float.BYTES * 4);
+        
+        specularColor.get(buffer);
+        buffer.position(buffer.position() + Float.BYTES * 4);
+    
+        buffer.putFloat(specularShininess);
+    
+        buffer.putFloat(padding[0]);
+        buffer.putFloat(padding[1]);
+        buffer.putFloat(padding[2]);
+        
+        return buffer;
+    }
+}
