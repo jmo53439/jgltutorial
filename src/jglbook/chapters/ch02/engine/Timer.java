9afc2d48fbd238e47a79e612f41541c77cd186bc
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-07-12 08:11:35 -0500

    Create Timer.java

---
+++ src/jglbook/chapters/ch02/engine/Timer.java
@@ -0,0 +1,30 @@
+package jglbook.chapters.ch02.engine;
+
+public class Timer {
+    
+    private double prevLoopTime;
+    
+    public void init() {
+        
+        prevLoopTime = getTime();
+    }
+    
+    public double getTime() {
+        
+        return System.nanoTime() / 1_000_000_000.0;
+    }
+    
+    public float getElapsedTime() {
+        
+        double time = getTime();
+        float elapsedTime = (float)(time - prevLoopTime);
+        prevLoopTime = time;
+        
+        return elapsedTime;
+    }
+    
+    public double getPrevLoopTime() {
+        
+        return prevLoopTime;
+    }
+}
