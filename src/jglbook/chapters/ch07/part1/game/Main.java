c737e20c14aae8efd8f003d440d66b938f561ca8
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-07-13 11:41:15 -0500

    Create Main.java

---
+++ src/jglbook/chapters/ch07/part1/game/Main.java
@@ -0,0 +1,23 @@
+package jglbook.chapters.ch07.part1.game;
+
+import jglbook.chapters.ch07.part1.engine.GameEngine;
+import jglbook.chapters.ch07.part1.engine.IGameLogic;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        try {
+            
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEngine = new GameEngine("Game", 600, 480, vSync, gameLogic);
+            gameEngine.start();
+        }
+        catch(Exception e) {
+            
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
